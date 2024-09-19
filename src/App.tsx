622f39ab312ab3c25e44ca8f9c90f7a4d533a500
Varsh1009 <83211221+Varsh1009@users.noreply.github.com>
2024-09-19 15:11:20 -0400

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Labs from './Labs';
+import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
+import Kanbas from './Kanbas';
+
+function App() {
+  return (
+    <HashRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Navigate to="Labs"/>}/>
+          <Route path="/Labs/*" element={<Labs />}/>
+          <Route path="/Kanbas/*" element={<Kanbas />} />
+          
+        </Routes>
+      </div>
+    </HashRouter>
+  );
+}
+
+export default App;
