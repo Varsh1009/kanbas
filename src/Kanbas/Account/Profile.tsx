2dd3f01cd530b60e406699da8c3d20770ddf5a5c
Varsh1009 <83211221+Varsh1009@users.noreply.github.com>
2024-09-19 18:18:15 -0400

    Update Profile.tsx

--- src/Kanbas/Account/Profile.tsx
+++ src/Kanbas/Account/Profile.tsx
@@ -5,12 +5,12 @@ export default function Profile()
     return (
         <div id="wd-profile-screen">
             <h3>Profile</h3>
-            <input id="wd-username" value="alice" placeholder="username" /> <br />
-            <input id="wd-password" value="123" placeholder="password" type="password" /> <br />
-            <input id="wd-firstname" value="Alice" placeholder="First Name" /> <br />
-            <input id="wd-lastname" value="Wonderland" placeholder="Last Name" /> <br />
+            <input id="wd-username" value="Kyle" placeholder="username" /> <br />
+            <input id="wd-password" value="123456" placeholder="password" type="password" /> <br />
+            <input id="wd-firstname" value="Kyle" placeholder="First Name" /> <br />
+            <input id="wd-lastname" value="Richards" placeholder="Last Name" /> <br />
             <input id="wd-dob" value="2024-01-01" type="date" /> <br />
-            <input id="wd-email" value="alice@wonderland" type="email" /> <br />
+            <input id="wd-email" value="Kyle@Richards" type="email" /> <br />
             <select id="wd-role">
                 <option value="USER">User</option>
                 <option value="ADMIN">Admin</option>
